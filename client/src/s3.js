7dfba4cc0a6e95bf80e33800ca2bbb2ab54f5230
Forrest <forrestzhang107@gmail.com>
2019-10-05 19:34:04 -0700

    s3 upload/download

---
+++ client/src/s3.js
@@ -0,0 +1,35 @@
+const aws = require('aws-sdk')
+const fs = require('fs')
+
+const s3 = new aws.S3({
+  region: 'us-west-1',
+  accessKeyId: 'AKIA5BSLTI7EMW34YDNJ',
+  secretAccessKey: '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'
+})
+
+const path = 'videos/'
+
+function uploadFile(file, filename) {
+  const params = {
+    Bucket: '5g-hackathon',
+    Key: path + filename,
+    Body: file
+  }
+  s3.upload(params, (e, data) => {
+    if (e) console.log(e)
+  })
+}
+
+
+async function getFile(filename) {
+  const params = {
+    Bucket: '5g-hackathon',
+    Key: path + filename,
+  }
+ s3.getObject(params, (e, data) => {
+    if (e) console.log(e)
+    console.log(data)
+  })
+}
+
+uploadFile('just a string', 'test3.txt')
